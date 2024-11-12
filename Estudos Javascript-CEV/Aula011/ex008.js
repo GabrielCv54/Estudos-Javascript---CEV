a1d695bd61fd7de898ac8808eb52d6caa91fc660
GabrielCv54 <gabriel.cvieira@aluno.faculdadeimpacta.com.br>
2024-11-12 18:32:17 -0300

    Add files via upload

---
+++ Estudos Javascript-CEV/Aula011/ex008.js
@@ -0,0 +1,8 @@
+var pais= 'Japão'
+console.log(`Natural de ${pais}`)
+
+if (pais=='Brasil'){
+    console.log('Brasileiro')
+}else{
+    console.log('estrangeiro')
+}
---
+++ Estudos Javascript-CEV/Aula012/ex011.js
@@ -0,0 +1,7 @@
+var nome='Johnny '
+
+if (nome=='Johnny'){
+    console.log(`Here's ${nome}`)
+}else{
+    console.log(`Seu nome é ${nome}`)
+}
