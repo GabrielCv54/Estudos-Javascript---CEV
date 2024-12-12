4915f617583e60d0f595682306a4ec42d5c2dcc8
GabrielCv54 <gabriel.cvieira@aluno.faculdadeimpacta.com.br>
2024-12-12 15:14:01 -0300

    Add files via upload

---
+++ Estudos Javascript-CEV/Matemática JS/aritmética.js
@@ -0,0 +1,31 @@
+/*let valor = 25
+let valor2 = 36
+let comparação = valor > valor2
+
+console.log(comparação)*/
+
+/*let a = 25
+let b = 105
+let c = a % b
+
+console.log(c)*/
+
+
+/*let x = 5
+x++
+let y = x
+
+console.log(y)*/
+
+/*let x = 88
+x--
+let z = x
+console.log(z)*/
+
+/*let x = 15
+x %= 5
+alert(x)*/
+
+let z = -200
+z<<=2
+alert(z)
